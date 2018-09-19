55921806680541a9eb48e474fc8637ccff600411
koehr <n@koehr.in>
2018-09-19 14:00:00 +0200

    fix embedded videos on mobile layout

---
+++ src/.vuepress/components/yt-video.vue
@@ -0,0 +1,12 @@
+<template>
+  <youtube player-width="100%" player-height="auto" :video-id="videoId" />
+</template>
+
+<script>
+export default {
+  name: 'yt-video',
+  props: {
+    videoId: {type: String, required: true}
+  }
+}
+</script>
