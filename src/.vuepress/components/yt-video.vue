297428108f289406a53fe93e94a391ec246af916
koehr <n@koehr.in>
2018-10-29 14:58:04 +0100

    better video sizing

--- src/.vuepress/components/yt-video.vue
+++ src/.vuepress/components/yt-video.vue
@@ -1,5 +1,5 @@
 <template>
-  <youtube player-width="100%" player-height="auto" :video-id="videoId" />
+  <youtube :player-width="width" :player-height="height" :video-id="videoId" />
 </template>
 
 <script>
@@ -7,6 +7,16 @@ export default {
   name: 'yt-video',
   props: {
     videoId: {type: String, required: true}
+  },
+  computed: {
+    width () {
+      // TODO: support window resize?
+      const parentEl = this.$parent.$el
+      return parentEl ? parentEl.clientWidth : 640
+    },
+    height () {
+      return Math.round(this.width * 0.5625)
+    }
   }
 }
 </script>
